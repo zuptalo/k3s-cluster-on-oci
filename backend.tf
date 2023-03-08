608bc4848210b6bf6efbe86ffd704c0487c4c6ff
Kamran Alipour <kamran.alipour@gmail.com>
2023-03-09 00:32:28 +0100

    updating terraform.yml

--- backend.tf
+++ backend.tf
@@ -1,15 +1,8 @@
 terraform {
   backend "s3" {
-    bucket                      = "terraform-state"
-    key                         = "terraform.tfstate"
-    region                      = "eu-stockholm-1"
-    endpoint                    = "https://objectstorage.eu-stockholm-1.oraclecloud.com/p/clJVTe-qMGJ9FZk7yecOVrqJVYsx1FBBpgGzYd6iLOrb8iGC734G7ZoCa7CAZ4G5/n/axquvpqvm9ie/b/terraform-state/o/"
-
-    # Set the pre-authenticated request details
-    preauthenticated_request {
-      description  = "Terraform State Storage"
-      access_type  = "ObjectReadWrite"
-      time_expires = "2030-12-31T23:59:00Z"
-    }
+    bucket   = "terraform-state"
+    key      = "terraform.tfstate"
+    region   = "eu-stockholm-1"
+    endpoint = "https://objectstorage.eu-stockholm-1.oraclecloud.com/p/clJVTe-qMGJ9FZk7yecOVrqJVYsx1FBBpgGzYd6iLOrb8iGC734G7ZoCa7CAZ4G5/n/axquvpqvm9ie/b/terraform-state/o/"
   }
 }
