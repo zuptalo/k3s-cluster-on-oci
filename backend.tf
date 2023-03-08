7f9d8cb816ffd23c196fd111e6d6caa18a37847e
Kamran Alipour <kamran.alipour@gmail.com>
2023-03-09 00:31:29 +0100

    updating terraform.yml

--- backend.tf
+++ backend.tf
@@ -4,12 +4,6 @@ terraform {
     key                         = "terraform.tfstate"
     region                      = "eu-stockholm-1"
     endpoint                    = "https://objectstorage.eu-stockholm-1.oraclecloud.com/p/clJVTe-qMGJ9FZk7yecOVrqJVYsx1FBBpgGzYd6iLOrb8iGC734G7ZoCa7CAZ4G5/n/axquvpqvm9ie/b/terraform-state/o/"
-    skip_credentials_validation = true
-    skip_get_ec2_platforms      = true
-    skip_requesting_account_id  = true
-
-    # Configure pre-authenticated requests
-    use_preauthenticated_requests = true
 
     # Set the pre-authenticated request details
     preauthenticated_request {
