5a68d8cdb76ed898ff851fae36d17dbb275cf32d
Kamran Alipour <kamran.alipour@gmail.com>
2023-03-09 00:53:23 +0100

    updating terraform.yml

--- backend.tf
+++ backend.tf
@@ -1,5 +1,6 @@
 terraform {
   backend "http" {
     address       = "https://objectstorage.eu-stockholm-1.oraclecloud.com/p/OjhagtZ4ZcbsQZNyxdkJ5erCy90SOO5pVus5OCLKgGQwDIaHsiuQZOeVSl-jn9DG/n/axquvpqvm9ie/b/terraform-state/o/"
+    update_method = "PUT"
   }
 }
