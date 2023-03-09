e455fc1b274f4abb7fe3889301c8e1fc39d7087f
Kamran Alipour <kamran.alipour@gmail.com>
2023-03-09 01:04:25 +0100

    updating terraform.yml

--- backend.tf
+++ backend.tf
@@ -9,8 +9,6 @@ terraform {
     skip_get_ec2_platforms      = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
-    skip_requesting_account_id  = true
-    skip_get_ec2_platforms      = true
     acl                         = "private"
     server_side_encryption_configuration {
       rule {
