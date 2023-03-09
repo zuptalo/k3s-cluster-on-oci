9c5b8fadab8db209c7a02fe6ba12157be6441b43
Kamran Alipour <kamran.alipour@gmail.com>
2023-03-09 01:40:36 +0100

    updating terraform.yml

--- backend.tf
+++ backend.tf
@@ -1,12 +1,5 @@
 terraform {
-  backend "s3" {
-    bucket                      = "terraform-state"
-    region                      = "eu-stockholm-1"
-    endpoint                    = "https://objectstorage.eu-stockholm-1.oraclecloud.com/p/Bu_KtpHn9YqAX39N7ef7cgtc3aC75VJPPYa5i0RULXJQjThLTOljo0VxrBCeH3aH/n/axquvpqvm9ie/b/terraform-state/o/"
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-    skip_region_validation      = true
-    acl                         = "private"
-    workspace_key_prefix        = "terraform-state"
+  backend "pg" {
+    conn_str = "postgres://pg.zuptalo.com:5433/terraform_state_db?user=terraform&password=Xman@1360"
   }
 }
