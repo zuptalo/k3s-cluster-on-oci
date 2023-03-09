0760490db3c8b37cbff9fcddd69be17d1dba8d08
Kamran Alipour <kamran.alipour@gmail.com>
2023-03-09 01:20:37 +0100

    updating terraform.yml

--- backend.tf
+++ backend.tf
@@ -1,13 +1,12 @@
 terraform {
   backend "s3" {
     bucket                      = "terraform-state"
-    key                         = "terraform.tfstate"
     region                      = "eu-stockholm-1"
-    endpoint                    = "https://objectstorage.eu-stockholm-1.oraclecloud.com/p/OjhagtZ4ZcbsQZNyxdkJ5erCy90SOO5pVus5OCLKgGQwDIaHsiuQZOeVSl-jn9DG/n/axquvpqvm9ie/b/terraform-state/o/"
+    endpoint                    = "https://objectstorage.eu-stockholm-1.oraclecloud.com/p/Bu_KtpHn9YqAX39N7ef7cgtc3aC75VJPPYa5i0RULXJQjThLTOljo0VxrBCeH3aH/n/axquvpqvm9ie/b/terraform-state/o/"
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
     acl                         = "private"
-    workspace_key_prefix        = "tfstate"
+    workspace_key_prefix        = "terraform-state"
   }
 }
