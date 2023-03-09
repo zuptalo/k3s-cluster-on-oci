aeb3cb1c550a518a8a39432d5a7375ce090f67e9
Kamran Alipour <kamran.alipour@gmail.com>
2023-03-09 01:02:59 +0100

    updating terraform.yml

--- backend.tf
+++ backend.tf
@@ -1,6 +1,25 @@
 terraform {
-  backend "http" {
-    address       = "https://objectstorage.eu-stockholm-1.oraclecloud.com/p/OjhagtZ4ZcbsQZNyxdkJ5erCy90SOO5pVus5OCLKgGQwDIaHsiuQZOeVSl-jn9DG/n/axquvpqvm9ie/b/terraform-state/o/"
-    update_method = "PUT"
+  backend "s3" {
+    bucket                      = "terraform-state"
+    key                         = "terraform.tfstate"
+    region                      = "eu-stockholm-1"
+    endpoint                    = "https://objectstorage.eu-stockholm-1.oraclecloud.com"
+    skip_requesting_account_id  = true
+    skip_credentials_validation = true
+    skip_get_ec2_platforms      = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_get_ec2_platforms      = true
+    acl                         = "private"
+    server_side_encryption_configuration {
+      rule {
+        apply_server_side_encryption_by_default {
+          sse_algorithm = "AES256"
+        }
+      }
+    }
+    workspace_key_prefix  = "tfstate"
+    pre_authenticated_url = "https://objectstorage.eu-stockholm-1.oraclecloud.com/p/OjhagtZ4ZcbsQZNyxdkJ5erCy90SOO5pVus5OCLKgGQwDIaHsiuQZOeVSl-jn9DG/n/axquvpqvm9ie/b/terraform-state/o/"
   }
 }
