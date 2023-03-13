ffec46fad552776625535ad931ed46b9965f795b
Zuptalo <kamran.alipour@gmail.com>
2023-03-13 22:46:52 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,13 +1,12 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state"
     key            = "terraform.tfstate"
     region         = "eu-north-1"
     endpoint       = "s3.zuptalo.com"
     access_key     = "terraform"
     secret_key     = "terraform"
     skip_requesting_account_id = true
-    skip_credentials_validation = false
+    skip_credentials_validation = true
     skip_get_ec2_platforms = true
     skip_metadata_api_check = true
     skip_region_validation = true
