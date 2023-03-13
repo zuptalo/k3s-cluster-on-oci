a65033ce04074b597cf36e13a2b3c1491251c005
Zuptalo <kamran.alipour@gmail.com>
2023-03-13 22:44:00 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -6,5 +6,10 @@ terraform {
     endpoint       = "s3.zuptalo.com"
     access_key     = "terraform"
     secret_key     = "terraform"
+    skip_requesting_account_id = true
+    skip_credentials_validation = false
+    skip_get_ec2_platforms = true
+    skip_metadata_api_check = true
+    skip_region_validation = true
   }
 }
