87f00f5282f92224cb62022885f8d99a7560aa21
Kamran Alipour <kamran.alipour@gmail.com>
2023-03-13 23:13:15 +0100

    updating backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,12 @@
 terraform {
   backend "s3" {
-    key            = "terraform.tfstate"
-    region         = "eu-north-1"
     endpoint       = "s3.zuptalo.com"
+    bucket         = "terraform-state"
+    key            = "/dev/terraform.tfstate"
+    region         = "eu-north-1"
     access_key     = "terraform"
     secret_key     = "terraform"
+    skip_metadata_api_check = true
+    skip_credentials_validation = true
   }
 }
