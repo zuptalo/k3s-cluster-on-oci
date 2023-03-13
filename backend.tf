ce73eae5834965f325734f58933106fd3d1968ac
Zuptalo <kamran.alipour@gmail.com>
2023-03-13 20:33:07 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "terraform-state"
     key            = "terraform.tfstate"
-    region         = "se-stockholm-1"
+    region         = "eu-north-1"
     endpoint       = "s3.zuptalo.com"
     access_key     = "r5K7SmnEqra4aOkn"
     secret_key     = "SnBD7AXJZlhF22s8cmDoCYqCS0GHTIeQ"
