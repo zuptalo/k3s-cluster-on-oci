131681d18e63c5ac87f783d4c3aa1b3e95aef2a3
Zuptalo <kamran.alipour@gmail.com>
2023-03-13 22:36:43 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,7 +4,7 @@ terraform {
     key            = "terraform.tfstate"
     region         = "eu-north-1"
     endpoint       = "s3.zuptalo.com"
-    access_key     = "r5K7SmnEqra4aOkn"
-    secret_key     = "SnBD7AXJZlhF22s8cmDoCYqCS0GHTIeQ"
+    access_key     = "terraform"
+    secret_key     = "terraform"
   }
 }
